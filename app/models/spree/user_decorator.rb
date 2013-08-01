16c0c0ea952597ca3a329b9be9a68ffd654520b5
Nizar Noorani <nizar.noorani@gmail.com>
2013-08-01 13:57:45 -0500

    sets user as owner on organization if owner is missing on org. this allows us to set the user as org owner when adding a new org.

--- app/models/spree/user_decorator.rb
+++ app/models/spree/user_decorator.rb
@@ -2,8 +2,14 @@ module Spree
   Spree::User.class_eval do 
     belongs_to :organization
     
+    before_validation :set_org_owner_if_nil
+    
     attr_accessible :organization_attributes
     
     accepts_nested_attributes_for :organization
+    
+    def set_org_owner_if_nil 
+      self.organization.owner = self if self.organization && !self.organization.owner
+    end
   end
 end
