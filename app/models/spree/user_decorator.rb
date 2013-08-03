171a6375d48dfb49798ec014187a16ce151906bf
Nizar Noorani <nizar.noorani@gmail.com>
2013-08-02 20:55:25 -0500

    fixes before_validation on user decorator to use if condition

--- app/models/spree/user_decorator.rb
+++ app/models/spree/user_decorator.rb
@@ -2,14 +2,14 @@ module Spree
   Spree::User.class_eval do 
     belongs_to :organization
     
-    before_validation :set_org_owner_if_nil
+    before_validation :set_as_org_owner, if: "organization && !organization.owner"
     
     attr_accessible :organization_attributes
     
     accepts_nested_attributes_for :organization
     
-    def set_org_owner_if_nil 
-      self.organization.owner = self if self.organization && !self.organization.owner
+    def set_as_org_owner
+      self.organization.owner = self 
     end
   end
 end
